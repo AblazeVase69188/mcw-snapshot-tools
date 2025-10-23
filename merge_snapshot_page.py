657895d8ae5050194a4d35766fc8b35081a64735
AblazeVase69188 <73418189+AblazeVase69188@users.noreply.github.com>
2025-10-23 11:34:28 +0800

    现在不是第一次出现的“; ”，其内容会被<!---->包围，以便手工处理；更改中有在新内容出现的“; ”，会被移动到新内容，但不一定正确，内容也会被<!---->包围，需要手工处理；修复了otherissuescount可能无法正确读取、otherissueslist可能无法正确生成的问题

--- merge_snapshot_page.py
+++ merge_snapshot_page.py
@@ -1,7 +1,7 @@
 import json
 import requests
 import sys
-
+'''
 MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest.json"
 WIKI_API_URL = "https://zh.minecraft.wiki/api.php"
 
@@ -44,6 +44,8 @@ if snapshot_list == []:
     input("按回车键退出")
     sys.exit(1)
 
+print(f"已找到{len(snapshot_list)}个快照")
+
 # 调整为Wiki页面标题
 for i, snapshot in enumerate(snapshot_list):
     if "-pre" in snapshot or "-rc" in snapshot:
@@ -73,7 +75,9 @@ raw_pages = [
     for page in snapshot_page_json["query"]["pages"]
     if "missing" not in page
 ]
-'''
+
+print("已成功获取所有快照页面内容，正在处理中")
+
 # 临时功能：将API结果保存以便参考
 try:
     output_filename = f"{release}.json"
@@ -82,10 +86,10 @@ try:
     print(f"已将内容成功保存到文件：{output_filename}")
 except Exception as e:
     print(f"保存文件时出错：{e}")
-
+'''
 # 临时功能：加载保存的API结果
 try:
-    input_filename = "merge_snapshot_page_test.json"
+    input_filename = "1.21.9.json"
     with open(input_filename, "r", encoding="utf-8") as f:
         raw_pages = json.load(f)
     print(f"已成功加载{len(raw_pages)}个页面。")
@@ -97,7 +101,7 @@ except Exception as e:
     print(f"读取文件时出错：{e}")
     input("按回车键退出")
     sys.exit(1)
-'''
+
 # 获取{{Undecided translation}}参数列表
 ut_text = None
 ut_params = set()
@@ -120,8 +124,8 @@ if ut_params:
 additions_marker = "== 新内容 =="
 changes_marker = "== 更改 =="
 fixes_marker = "== 修复 =="
-end_markers = ["== 参考 ==", "== 导航 =="]
-feature_markers = ["=== 方块 ===", "=== 物品 ===", "=== 生物 ===", "=== 非生物实体 ===", "=== 世界生成 ===", "=== 游戏内容 ===", "=== 命令格式 ===", "=== 常规 ==="]
+end_marker_list = ["== 参考 ==", "== 导航 =="]
+feature_marker_list = ["=== 方块 ===", "=== 物品 ===", "=== 生物 ===", "=== 非生物实体 ===", "=== 世界生成 ===", "=== 游戏内容 ===", "=== 命令格式 ===", "=== 常规 ==="]
 merged_features_data = [
     # 新内容
     [
@@ -153,7 +157,7 @@ merged_fixes_data = {
     "new": 1,
     "otherissuescount": 0,
     "issues": {},
-    "otherissues": []
+    "otherissues": {}
 }
 
 for i, page_content in enumerate(raw_pages):
@@ -163,8 +167,8 @@ for i, page_content in enumerate(raw_pages):
     fixes_index = page_content.find(fixes_marker)
 
     end_index = -1
-    for marker in end_markers:
-        idx = page_content.find(marker)
+    for end_marker in end_marker_list:
+        idx = page_content.find(end_marker)
         if idx != -1:
             if end_index == -1 or idx < end_index:
                 end_index = idx
@@ -192,17 +196,17 @@ for i, page_content in enumerate(raw_pages):
 
     # 解析“新内容”部分
     if additions_text:
-        feature_indexes = [additions_text.find(marker) for marker in feature_markers]
+        feature_indexes = [additions_text.find(feature_marker) for feature_marker in feature_marker_list]
         if not any(index != -1 for index in feature_indexes):
             print(f"{snapshot_list[i]}“新内容”段落不符合格式指导，请检查。")
             input("按回车键退出")
             sys.exit(1)
         
-        for i, index in enumerate(feature_indexes):
+        for j, index in enumerate(feature_indexes):
             if index == -1:
                 continue
             else:
-                next_indexes = [idx for idx in feature_indexes[i+1:] if idx != -1]
+                next_indexes = [idx for idx in feature_indexes[j+1:] if idx != -1]
                 end_index = next_indexes[0] if next_indexes else len(additions_text)
                 raw_text = additions_text[index:end_index].strip()
 
@@ -218,7 +222,10 @@ for i, page_content in enumerate(raw_pages):
 
                     if line.startswith('; '):
                         if current_title and current_content:  # 并非首个假标题
-                            merged_features_data[0][i][current_title] = current_content
+                            if current_title in merged_features_data[0][j]:
+                                merged_features_data[0][j][current_title].append(current_content)
+                            else:
+                                merged_features_data[0][j][current_title] = current_content
                         # 获取当前假标题，并重置内容列表
                         current_title = line[2:].strip()
                         current_content = []
@@ -229,21 +236,24 @@ for i, page_content in enumerate(raw_pages):
 
                 # 添加最后一个假标题及其内容
                 if current_title and current_content:
-                    merged_features_data[0][i][current_title] = current_content
+                    if current_title in merged_features_data[0][j]:
+                        merged_features_data[0][j][current_title].append(current_content)
+                    else:
+                        merged_features_data[0][j][current_title] = current_content
             
     # 解析“更改”部分
     if changes_text:
-        feature_indexes = [changes_text.find(marker) for marker in feature_markers]
+        feature_indexes = [changes_text.find(feature_marker) for feature_marker in feature_marker_list]
         if not any(index != -1 for index in feature_indexes):
             print(f"{snapshot_list[i]}“更改”段落不符合格式指导，请检查。")
             input("按回车键退出")
             sys.exit(1)
         
-        for i, index in enumerate(feature_indexes):
+        for j, index in enumerate(feature_indexes):
             if index == -1:
                 continue
             else:
-                next_indexes = [idx for idx in feature_indexes[i+1:] if idx != -1]
+                next_indexes = [idx for idx in feature_indexes[j+1:] if idx != -1]
                 end_index = next_indexes[0] if next_indexes else len(changes_text)
                 raw_text = changes_text[index:end_index].strip()
 
@@ -259,7 +269,17 @@ for i, page_content in enumerate(raw_pages):
 
                     if line.startswith('; '):
                         if current_title and current_content:  # 并非首个假标题
-                            merged_features_data[1][i][current_title] = current_content
+                            if current_title in merged_features_data[1][j]:  # 之前已经检查过，确定不应该移去“新内容”
+                                merged_features_data[1][j][current_title].append(current_content)
+                            else:
+                                ismoved = False
+                                for title in merged_features_data[0][j]:
+                                    if current_title == title:
+                                        merged_features_data[0][j][current_title].append(current_content)
+                                        ismoved = True
+                                        break
+                                if not ismoved:
+                                    merged_features_data[1][j][current_title] = current_content
                         # 获取当前假标题，并重置内容列表
                         current_title = line[2:].strip()
                         current_content = []
@@ -270,27 +290,37 @@ for i, page_content in enumerate(raw_pages):
                 
                 # 添加最后一个假标题及其内容
                 if current_title and current_content:
-                    merged_features_data[1][i][current_title] = current_content
+                    if current_title in merged_features_data[1][j]:  # 之前已经检查过，确定不应该移去“新内容”
+                        merged_features_data[1][j][current_title].append(current_content)
+                    else:
+                        ismoved = False
+                        for title in merged_features_data[0][j]:
+                            if current_title == title:
+                                merged_features_data[0][j][current_title].append(current_content)
+                                ismoved = True
+                                break
+                        if not ismoved:
+                            merged_features_data[1][j][current_title] = current_content
 
     # 解析“修复”部分
     if fixes_text:
         fixes_start_index = fixes_text.find("{{fixes|")
         if fixes_start_index == -1:
-            print(f"{snapshot_list[i]}“修复”段落中缺少{{fixes}}模板，请检查。")
+            print(f"{snapshot_list[i]}“修复”段落中缺少{{{{fixes}}}}模板，请检查。")
             input("按回车键退出")
             sys.exit(1)
 
         matches = 0
-        for i, char in enumerate(fixes_text[fixes_start_index:]):
+        for j, char in enumerate(fixes_text[fixes_start_index:]):
             if char == '{':
                 matches += 1
             elif char == '}':
                 matches -= 1
             if matches == 0:
-                fixes_end_index = fixes_start_index + i + 1
+                fixes_end_index = fixes_start_index + j + 1
                 break
         if matches != 0:
-            print(f"{snapshot_list[i]}“修复”段落中的{{fixes}}模板未闭合，请检查。")
+            print(f"{snapshot_list[i]}“修复”段落中的{{{{fixes}}}}模板未闭合，请检查。")
             input("按回车键退出")
             sys.exit(1)
             
@@ -301,7 +331,7 @@ for i, page_content in enumerate(raw_pages):
         # 获取beforeVersion、fixedin和otherissuescount参数
         fixedin_start_index = fixes_params.find("|fixedin=")
         if fixedin_start_index == -1:
-            print(f"{snapshot_list[i]}“修复”段落中的{{fixes}}模板缺少fixedin参数，请检查。")
+            print(f"{snapshot_list[i]}“修复”段落中的{{{{fixes}}}}模板缺少fixedin参数，请检查。")
             input("按回车键退出")
             sys.exit(1)
             
@@ -319,13 +349,21 @@ for i, page_content in enumerate(raw_pages):
         otherissuescount_value = None
         otherissuescount_start_index = fixes_params.find("|otherissuescount=")
         if otherissuescount_start_index != -1:
-            otherissuescount_start_index = otherissuescount_start_index + len("|otherissuescount=")
+            otherissuescount_start_index += len("|otherissuescount=")
+            
+            # 查找"|"或"}}"
             otherissuescount_end_index = fixes_params.find("|", otherissuescount_start_index)
-            if otherissuescount_end_index == -1:
-                otherissuescount_value = fixes_params[otherissuescount_start_index:].strip()
-            else:
+            if otherissuescount_end_index != -1:
                 otherissuescount_value = fixes_params[otherissuescount_start_index:otherissuescount_end_index].strip()
-            merged_fixes_data['otherissuescount'] += int(otherissuescount_value)
+            else:
+                otherissuescount_end_index = fixes_params.find("}}", otherissuescount_start_index)
+                if otherissuescount_end_index != -1:
+                    otherissuescount_value = fixes_params[otherissuescount_start_index:otherissuescount_end_index].strip()
+                else:
+                    otherissuescount_value = fixes_params[otherissuescount_start_index:].strip()
+
+            if otherissuescount_value:
+                merged_fixes_data['otherissuescount'] += int(otherissuescount_value)
 
         # 获取漏洞列表
         current_title = ""
@@ -354,14 +392,33 @@ for i, page_content in enumerate(raw_pages):
                 merged_fixes_data['issues'][current_title][bug_id] = bug_desc
 
         # 解析“其他漏洞”部分
-        otherissues_start_index = fixes_text.find("; 其他漏洞")
-        if otherissues_start_index != -1:
-            otherissues_text = fixes_text[otherissues_start_index + len("; 其他漏洞"):]
-            merged_fixes_data['otherissues'].extend(line.strip() for line in otherissues_text.splitlines() if line.strip() and line.strip() != '')
-            if not otherissuescount_value:
-                print(f"{snapshot_list[i]}“修复”段落中的{{fixes}}模板缺少otherissuescount参数，请检查。")
-                input("按回车键退出")
-                sys.exit(1)
+        fixesother_text = fixes_text[fixes_end_index:]
+        if fixesother_text.strip() and otherissuescount_value is not None:
+            lines = fixesother_text.strip().splitlines()
+            current_title = ""
+            current_content = []
+            for line in lines:
+                if line.startswith('; '):
+                    if current_title and current_content:
+                        if current_title in merged_fixes_data['otherissues']:
+                            merged_fixes_data['otherissues'][current_title].extend(current_content)
+                        else:
+                            merged_fixes_data['otherissues'][current_title] = current_content
+                    current_title = line[2:].strip()
+                    current_content = []
+                else:
+                    stripped_line = line.strip()
+                    if stripped_line:
+                        current_content.append(stripped_line)
+            
+            if current_title and current_content:
+                if current_title in merged_fixes_data['otherissues']:
+                    merged_fixes_data['otherissues'][current_title].extend(current_content)
+                else:
+                    merged_fixes_data['otherissues'][current_title] = current_content
+
+# print(json.dumps(merged_features_data, ensure_ascii=False, indent=4))
+# print(json.dumps(merged_fixes_data, ensure_ascii=False, indent=4))
 
 # 打印
 print("合并后的页面，内容为：")
@@ -370,7 +427,7 @@ if ut_text:
     print(ut_text, end='\n\n')
 
 for i, second_level_item in enumerate(merged_features_data):
-    if not second_level_item:
+    if not any(second_level_item):
         continue
 
     if i == 0:
@@ -380,11 +437,21 @@ for i, second_level_item in enumerate(merged_features_data):
     
     for j, third_level_item in enumerate(second_level_item):
         if third_level_item:
-            print(feature_markers[j])
-            for title, content in third_level_item.items():
+            print(feature_marker_list[j])
+            for title, content_list in third_level_item.items():
                 print(f"; {title}")
-                for line in content:
+                init = [x for x in content_list if not isinstance(x, list)]
+                after = [s for sub in content_list if isinstance(sub, list) for s in sub]
+                # 打印首批内容
+                for line in init:
                     print(line)
+                
+                # 后续内容手工处理
+                if after:
+                    print("<!--")
+                    for content in after:
+                        print(content)
+                    print("-->")
                 print()
 
 if merged_fixes_data['issues'] or merged_fixes_data['otherissues']:
@@ -393,24 +460,31 @@ if merged_fixes_data['issues'] or merged_fixes_data['otherissues']:
     fixes_params = [f"fixedin={','.join(merged_fixes_data['fixedin'])}"]
     if len(merged_fixes_data['fixedin']) > 1:
         fixes_params.append(f"beforeVersion={merged_fixes_data['beforeVersion']}")
-    fixes_params.extend(["showdesc=1", "new=1"])
+    
+    if merged_fixes_data['issues']:
+        fixes_params.extend(["showdesc=1", "new=1"])
 
     if merged_fixes_data['otherissuescount'] != 0:
         fixes_params.append(f"otherissuescount={merged_fixes_data['otherissuescount']}")
 
-    print("{{fixes|" + '|'.join(fixes_params))
+    print("{{fixes|" + '|'.join(fixes_params), end="")
 
-    for category, bugs in merged_fixes_data['issues'].items():
-        print(f"|{category}")
-        
-        sorted_bugs = sorted(bugs.items(), key=lambda item: int(item[0]))
-        
-        for bug_id, desc in sorted_bugs:
-            print(f"|{bug_id}|{desc}")
+    # TODO：“|;1.21.x的漏洞”需要按x排序
+
+    if merged_fixes_data['issues']:
+        print()
+        for category, bugs in merged_fixes_data['issues'].items():
+            print(f"|{category}")
+            
+            sorted_bugs = sorted(bugs.items(), key=lambda item: int(item[0]))
+            
+            for bug_id, desc in sorted_bugs:
+                print(f"|{bug_id}|{desc}")
 
     print("}}")
 
     if merged_fixes_data['otherissues']:
-        print("; 其他漏洞")
-        for issue in merged_fixes_data['otherissues']:
-            print(issue)
+        for title, issues in merged_fixes_data['otherissues'].items():
+            print(f"; {title}")
+            for issue in issues:
+                print(issue)
